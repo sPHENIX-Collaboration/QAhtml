2f787719fc4029fb4d5a30fdf86d5d9420742974
rosstom <rosstom@ucla.edu>
2024-06-04 15:46:47 -0400

    Adding new MVTX and INTT QAHtml modules

---
+++ subsystems/intt/INTTDraw.h
@@ -0,0 +1,35 @@
+#ifndef INTT_INTTDRAW_H__
+#define INTT_INTTDRAW_H__
+
+#include <qahtml/QADraw.h>
+
+#include <vector>
+
+class QADB;
+class QADBVar;
+class TCanvas;
+class TGraphErrors;
+class TPad;
+class TH1F;
+class TH2F;
+
+class INTTDraw : public QADraw
+{
+ public: 
+  INTTDraw(const std::string &name = "INTTQA");
+  ~INTTDraw() override;
+
+  int Draw(const std::string &what = "ALL") override;
+  int MakeHtml(const std::string &what = "ALL") override;
+  int DBVarInit();
+
+ private:
+  int MakeCanvas(const std::string &name, int num);
+  int DrawChipInfo();
+  TCanvas *TC[1]{};
+  TPad *transparent[1]{};
+  TPad *Pad[1][2]{};
+  const char *histprefix;
+};
+
+#endif
