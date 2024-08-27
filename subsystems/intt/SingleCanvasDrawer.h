81fba089d922cb0cb837375ba06a0f19c8ffca62
josephbertaux <jbertau@purdue.edu>
2024-08-27 12:33:54 -0400

    Added base class for drawing options, implemented current options as derived classes, and rewrote client to handle a map of options as pointer-to-base class

---
+++ subsystems/intt/SingleCanvasDrawer.h
@@ -0,0 +1,25 @@
+#ifndef SINGLE_CANVAS_DRAWER_H
+#define SINGLE_CANVAS_DRAWER_H
+
+#include <string>
+#include <TCanvas.h>
+
+class SingleCanvasDrawer
+{
+public:
+  SingleCanvasDrawer(std::string const& name) {m_name = name;}
+  virtual ~SingleCanvasDrawer() {delete m_canvas;}
+
+  TCanvas* GetCanvas() {return m_canvas;}
+  std::string GetName() {return m_name;}
+
+  virtual int DrawCanvas();
+
+protected:
+  virtual int MakeCanvas();
+
+  TCanvas* m_canvas{nullptr};
+  std::string m_name;
+};
+
+#endif//SINGLE_CANVAS_DRAWER_H
