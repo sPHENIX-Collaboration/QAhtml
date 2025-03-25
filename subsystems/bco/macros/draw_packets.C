b6aea4167c0e49a1e663354dee9f7e4268fb7718
Joe Osborn <osbornjd91@gmail.com>
2025-03-25 16:31:02 -0400

    bco draw uses aggregated roc files

--- subsystems/bco/macros/draw_packets.C
+++ subsystems/bco/macros/draw_packets.C
@@ -1,18 +1,18 @@
 #include <qahtml/QADrawClient.h>
 #include <bco/BCODraw.h>
-#include <sPhenixStyle.C>
+//#include <sPhenixStyle.C>
 R__LOAD_LIBRARY(libqadrawbco.so)
 
-void draw_packets(const std::string &rootfile) {
+void draw_packets(const std::string &rootfile, const std::string& what) {
   SetsPhenixStyle();
   QADrawClient *cl = QADrawClient::instance();
-  /* cl->Verbosity(1); */
+  cl->Verbosity(0); 
   QADraw *ex = new BCODraw();
   cl->registerDrawer(ex);
 
   cl->ReadHistogramsFromFile(rootfile);
   /* cl->Print("ALL"); */
-  cl->MakeHtml();
+  cl->MakeHtml(what, "BCOQA");
   delete cl;
   std::cout << "Done" << std::endl;
   gSystem->Exit(0);
