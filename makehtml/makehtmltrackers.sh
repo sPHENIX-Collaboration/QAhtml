049f9dd223cae71e7ba1ed1ac3a34d9cbaacc9f8
osbornjd <osbornjd91@gmail.com>
2025-03-27 15:54:00 -0400

    better parallel processing enabled

--- makehtml/makehtmltrackers.sh
+++ makehtml/makehtmltrackers.sh
@@ -3,8 +3,19 @@
 histtype=$1
 echo $$ > htmlrunningtrk$histtype
 source ./setup_all.sh
-Xvfb :2 -nolisten tcp &
-export DISPLAY=unix:2
+if [ $histtype = "hit" ]; then
+    Xvfb :2 -nolisten tcp &
+    export DISPLAY=unix:2
+elif [ $histtype = "cluster" ]; then
+    Xvfb :3 -nolisten tcp &
+    export DISPLAY=unix:3
+elif [ $histtype = "seed" ]; then
+    Xvfb :5 -nolisten tcp &
+    export DISPLAY=unix:4
+else
+    kill $!
+    rm htmlrunningtrk$histtype
+fi
 python3 makehtmltrackers.py -ht $histtype >& /sphenix/u/sphnxpro/qahtml/QAhtml/makehtml/makehtmltrack$histtype.log
 kill $!
 rm htmlrunningtrk$histtype
