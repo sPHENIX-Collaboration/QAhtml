f0eff2deb284a88d385b7b3239127817911b8fde
osbornjd <osbornjd91@gmail.com>
2025-03-27 14:19:45 -0400

    remove -v-t

--- makehtml/makehtmltrackers.sh
+++ makehtml/makehtmltrackers.sh
@@ -5,7 +5,7 @@ echo $$ > htmlrunningtrk$histtype
 source ./setup_all.sh
 Xvfb :2 -nolisten tcp &
 export DISPLAY=unix:2
-python3 makehtmltrackers.py -v -t -ht $histtype >& /sphenix/u/sphnxpro/qahtml/QAhtml/makehtml/makehtmltrack$histtype.log
+python3 makehtmltrackers.py -ht $histtype >& /sphenix/u/sphnxpro/qahtml/QAhtml/makehtml/makehtmltrack$histtype.log
 kill $!
 rm htmlrunningtrk$histtype
 # no echos - this will force an email from cron
