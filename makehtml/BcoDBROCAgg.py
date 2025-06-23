aac90ce0919d485bb15a2a483eaf56521bdf720c
osbornjd <osbornjd91@gmail.com>
2025-06-23 15:31:17 -0400

    fix file catalog issue

--- makehtml/BcoDBROCAgg.py
+++ makehtml/BcoDBROCAgg.py
@@ -92,7 +92,8 @@ def main():
                 if len(filesToAdd) == 0:
                     # nothing to add, move on
                     continue
-                
+                if filesToAdd[0].find("/") == -1:
+                    continue
                 tags = (filesToAdd[0]).split(os.sep)
                 index = tags.index("production")+1
                 collisiontag = tags[index]
