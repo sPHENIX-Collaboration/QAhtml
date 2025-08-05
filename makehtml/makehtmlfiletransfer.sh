f3a0e79f06f10d2506ce584748d13221dc3578aa
osbornjd <osbornjd91@gmail.com>
2025-08-05 12:01:42 -0400

    add scripts

---
+++ makehtml/makehtmlfiletransfer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+[[ -e htmlrunningfiletransfer ]] && exit 0
+echo $$ > htmlrunningfiletransfer
+source ./setup_all.sh
+ROOT_INCLUDE_PATH=${OPT_SPHENIX}/include:${ROOT_INCLUDE_PATH}
+Xvfb :7 -nolisten tcp &
+export DISPLAY=unix:7
+python3 makehtmlfiletransfer.py -v >& /sphenix/user/sphnxpro/htmllogs/makehtmlfiletransfer.log
+kill $!
+rm htmlrunningfiletransfer
+# no echos - this will force an email from cron
+#echo "Finished"
